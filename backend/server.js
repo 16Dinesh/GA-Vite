f443c766b5bdf393c115461cdeb41e2942cf4bbb
test <Dineshwonks@Gmail.com>
2024-09-24 15:11:45 +0530

    server code

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,10 @@
-if(process.env.NODE_ENV != "production") {
-    require('dotenv').config()
-}
+require('dotenv').config()
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-
-//Routes
-// const authRouter = require("./routes/auth/auth-routes");
-// const adminProductsRouter = require("./routes/admin/products-routes");
-// const adminOrderRouter = require("./routes/admin/order-routes");
-
-// const gaProductsRouter = require("./routes/ga/products-routes");
-// const gaCartRouter = require("./routes/ga/cart-routes");
-// const gaAddressRouter = require("./routes/ga/address-routes");
-// const gaOrderRouter = require("./routes/ga/order-routes");
-// const gaSearchRouter = require("./routes/ga/search-routes");
-// const gaReviewRouter = require("./routes/ga/review-routes");
-
-// const commonFeatureRouter = require("./routes/common/feature-routes");
-
 
 //DataBase
 const db_url = process.env.DB_URL
@@ -59,7 +42,7 @@ const sessionOptions = {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_BASE_URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
@@ -76,18 +59,10 @@ app.use(
 app.use(session(sessionOptions));
 app.use(cookieParser());
 app.use(express.json());
-// app.use("/api/auth", authRouter);
-// app.use("/api/admin/products", adminProductsRouter);
-// app.use("/api/admin/orders", adminOrderRouter);
 
-// app.use("/api/ga/products", gaProductsRouter);
-// app.use("/api/ga/cart", gaCartRouter);
-// app.use("/api/ga/address", gaAddressRouter);
-// app.use("/api/ga/order", gaOrderRouter);
-// app.use("/api/ga/search", gaSearchRouter);
-// app.use("/api/ga/review", gaReviewRouter);
-
-// app.use("/api/common/feature", commonFeatureRouter);
+app.get('/', (req, res )=> {
+  res.send("Hello")
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
