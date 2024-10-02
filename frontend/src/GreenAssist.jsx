7b715f5ec2b235789de60e2d8f75063bf073646e
test <Dineshwonks@Gmail.com>
2024-10-02 17:51:13 +0530

    scroll to top component

--- frontend/src/GreenAssist.jsx
+++ frontend/src/GreenAssist.jsx
@@ -1,25 +1,27 @@
+// GreenAssist.js
 import Home from "./pages/Home";
-import Services from './pages/Services'
-import Aboutus from './pages/Aboutus'
-import Beanassist from './pages/Beanassist'
-import Contactus from './pages/Contactus'
-import Privacypolicy from './pages/Privacypolicy'
-import Termsandconditions from './pages/Termsandconditions'
+import Services from './pages/Services';
+import Aboutus from './pages/Aboutus';
+import Beanassist from './pages/Beanassist';
+import Contactus from './pages/Contactus';
+import Privacypolicy from './pages/Privacypolicy';
+import Termsandconditions from './pages/Termsandconditions';
 import { Route, Routes } from "react-router-dom";
+import ScrollToTop from './components/common/ScrollToTop';
 
 export default function GreenAssist() {
-
     return (
         <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/aboutus" element={<Aboutus />} />
-          <Route path="/beanassist" element={<Beanassist />} />
-          <Route path="/contactus" element={<Contactus />} />
-          <Route path="/privacypolicy" element={<Privacypolicy />} />
-          <Route path="/termsandconditions" element={<Termsandconditions />} />
-        </Routes>
-      </div>
-    )
+            <ScrollToTop /> 
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/aboutus" element={<Aboutus />} />
+                <Route path="/beanassist" element={<Beanassist />} />
+                <Route path="/contactus" element={<Contactus />} />
+                <Route path="/privacypolicy" element={<Privacypolicy />} />
+                <Route path="/termsandconditions" element={<Termsandconditions />} />
+            </Routes>
+        </div>
+    );
 }
